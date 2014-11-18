04e8bf3efe421b33d9a042e39e66c1c67fd4498e
JoeSpinogatti <jps110@pitt.edu>
2014-11-18 02:01:27 -0500

    Working App, Downloads half work
    Need to change from external storage location to internal storage
    location

--- Cue/app/src/main/java/app/com/cue/MainActivity.java
+++ Cue/app/src/main/java/app/com/cue/MainActivity.java
@@ -10,15 +10,20 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * An {@link Activity} showing a tuggable "Hello World!" card.
@@ -104,9 +109,64 @@ public class MainActivity extends Activity {
     private View buildView() {
         CardBuilder card = new CardBuilder(this, CardBuilder.Layout.TEXT);
 
-        double q = 10.10;
-        
-        card.setText("This is cue: \n\n" + "              " + q);
+        int count;
+        try {
+            URL url = new URL("http://joespinogatti.com/cuetxt/q.txt");
+            URLConnection conexion = url.openConnection();
+            conexion.connect();
+            int lenghtOfFile = conexion.getContentLength();
+            InputStream is = url.openStream();
+            File testDirectory = new File(
+                    Environment.getExternalStorageDirectory() + "/Download");
+            if (!testDirectory.exists()) {
+                testDirectory.mkdir();
+            }
+            FileOutputStream fos = new FileOutputStream(testDirectory
+                    + "/q.txt");
+            byte data[] = new byte[1024];
+            long total = 0;
+            int progress = 0;
+            while ((count = is.read(data)) != -1) {
+                total += count;
+                int progress_temp = (int) total * 100 / lenghtOfFile;
+                    /*publishProgress("" + progress_temp); //only for asynctask
+                    if (progress_temp % 10 == 0 && progress != progress_temp) {
+                        progress = progress_temp;
+                    }*/
+                fos.write(data, 0, count);
+            }
+            is.close();
+            fos.close();
+        } catch (Exception e) {
+            Log.e("ERROR DOWNLOADING",
+                    "Unable to download" + e.getMessage());
+        }
+
+        //Find the directory for the SD Card using the API
+
+        File sdcard = Environment.getExternalStorageDirectory();
+
+//Get the text file
+        File file = new File(sdcard,"q.txt");
+
+//Read text from file
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+
+        card.setText("This is cue: \n\n" + "              " + text);
         // card.setText(R.string.insert_number);
         return card.getView();
     }
